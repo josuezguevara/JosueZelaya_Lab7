0ac137f7a24794a74c102a04312583230ddb1bd0
Josue Zelaya <josuezelaya99@hotmail.es>
2017-03-10 14:31:15 -0600

    Clase Dragon con su ataque

---
+++ JosueZelaya_Lab7/src/josuezelaya_lab7/Bestia.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package josuezelaya_lab7;
+
+/**
+ *
+ * @author usuario
+ */
+public abstract class Bestia extends Guerreros{
+    public abstract void AtaqueDragon(Guerreros guerrero);
+}
---
+++ JosueZelaya_Lab7/src/josuezelaya_lab7/Dragon.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package josuezelaya_lab7;
+
+/**
+ *
+ * @author usuario
+ */
+public class Dragon extends Bestia{
+    @Override
+    public void AtaqueDragon(Guerreros guerrero){
+        double salud_rival;
+        double poder_ataque, defensa_rival1, defensa_rival2,resta_salud;
+        poder_ataque=guerrero.getPoder_ataque()*0.35;//agaramos el 35%
+        salud_rival= (guerrero.getPoder_ataque()+poder_ataque)-guerrero.getPoder_defensa();//suma del 35% de atque y resta del poder defensa del enemigo
+        defensa_rival1=guerrero.getPoder_defensa()*0.15;//agarramos el 15% de su poder defensa
+        defensa_rival2=guerrero.getPoder_defensa()-defensa_rival1;
+        guerrero.setPoder_defensa(defensa_rival2);//se le resta el 15%
+        resta_salud=guerrero.getSalud()-salud_rival;//restamos la salud del rival 
+        guerrero.setSalud(resta_salud);//asignammos
+        guerrero.setPoder_ataque(poder_ataque);//asiganmos
+    }
+
+   
+}
