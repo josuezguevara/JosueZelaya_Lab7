ca212f8793fd4e049151a862ef7473aa0648560a
Josue Zelaya <josuezelaya99@hotmail.es>
2017-03-10 15:34:28 -0600

    Clase Alumno reconstruido con su ataque

--- JosueZelaya_Lab7/src/josuezelaya_lab7/Alumnos.java
+++ JosueZelaya_Lab7/src/josuezelaya_lab7/Alumnos.java
@@ -9,7 +9,7 @@ package josuezelaya_lab7;
  *
  * @author usuario
  */
-public class Alumnos extends Guerreros{
+public class Alumnos extends Bestia{
     private long num_cuenta;
     private String carrera;
     private int edad;
@@ -18,12 +18,14 @@ public class Alumnos extends Guerreros{
         super();
     }
 
-    public Alumnos(long num_cuenta, String carrera, int edad, String nombre, String nickname, double poder_ataque, double poder_defensa, double salud, double puntos) {
-        super(nombre, nickname, poder_ataque, poder_defensa, salud, puntos);
+    public Alumnos(long num_cuenta, String carrera, int edad) {
         this.num_cuenta = num_cuenta;
         this.carrera = carrera;
         this.edad = edad;
     }
+
+  
+    
 
     public long getNum_cuenta() {
         return num_cuenta;
@@ -52,6 +54,17 @@ public class Alumnos extends Guerreros{
     @Override
     public String toString() {
         return super.getNombre()+ num_cuenta;
+    }
+
+    @Override
+    public void Ataque(Guerreros guerrero) {
+        double vida_rival,resta,vida_alumno,porcentaje;
+        resta=this.getPoder_ataque()-guerrero.getPoder_defensa();
+        vida_rival=guerrero.getSalud()-resta;
+        guerrero.setSalud(vida_rival);//asignamos
+        porcentaje=this.getSalud()*0.25;
+        vida_alumno=this.getSalud()+porcentaje;
+        this.setSalud(vida_alumno);
     }
     
     
